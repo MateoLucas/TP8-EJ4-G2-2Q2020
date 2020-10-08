783a38a8beb5d409b02f55febbd4001e73f7fd29
MateoLucas <mlucas@itba.edu.ar>
2020-10-08 18:26:03 -0300

    TP8 0.0

---
+++ TP8/main.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: mateo
+ *
+ * Created on October 8, 2020, 6:23 PM
+ */
+
+#include <stdio.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+{
+    char a = 'h';
+    void *p;
+    p = &a;
+    printf("a: %c\n", *p);
+    return 0;
+}
+  
+
